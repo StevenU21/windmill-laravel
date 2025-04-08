ccb75226745b20541061992ce77e85956cc62381
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-04-08 17:23:18 -0600

    Add custom 500 error page with user-friendly messaging and layout

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
-@section('title', 'Page Not Found')
+@section('title', 'Server Error')
 
 @section('content')
     <div class="container flex flex-col items-center px-6 mx-auto">
-        <svg class="w-12 h-12 mt-8 text-purple-200" fill="currentColor" viewBox="0 0 20 20">
+        <svg class="w-12 h-12 mt-8 text-red-500" fill="currentColor" viewBox="0 0 20 20">
             <path fill-rule="evenodd"
-                d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z"
+                d="M18 10c0 4.418-3.582 8-8 8s-8-3.582-8-8 3.582-8 8-8 8 3.582 8 8zm-8-3a1 1 0 00-1 1v2a1 1 0 002 0V8a1 1 0 00-1-1zm0 6a1 1 0 100 2 1 1 0 000-2z"
                 clip-rule="evenodd"></path>
         </svg>
         <h1 class="text-6xl font-semibold text-gray-700 dark:text-gray-200">
-            404
+            500
         </h1>
-        <p class="text-gray-700 dark:text-gray-300">
-            Page not found. Check the address or
+        <p class="text-gray-700 dark:text-gray-300 text-center">
+            Oops! Something went wrong on our end. Please try again later or
             <a class="text-purple-600 hover:underline dark:text-purple-300" href="{{ url()->previous() }}">
                 go back
             </a>
