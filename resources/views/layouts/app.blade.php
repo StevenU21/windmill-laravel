fab97f4624e955605d2b54af24b149e90872dbbd
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-04-08 21:08:36 -0600

    Add FontAwesome CDN to app and guest layouts for icon support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,13 @@
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
       rel="stylesheet"
     />
+
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMt23cez/3paNdF+Z4p6F5r5V6M1fE7y3eG5eDk"
+      crossorigin="anonymous"
+    />
+
     <link rel="stylesheet" href="{{ asset('css/tailwind.output.css')}}" />
     <script
       src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,11 @@
       defer
     ></script>
     <script src="{{ asset('js/init-alpine.js')}}"></script>
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMt23cez/3paNdF+Z4p6F5r5V6M1fE7y3eG5eDk"
+      crossorigin="anonymous"
+    />
   </head>
   <body>
     <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
