e86027b5a3be52f3fdd4d9d393e236abfb8bdaf8
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-04-10 16:33:26 -0600

    Update sidebar navigation to highlight all category routes

--- resources/views/navigations/sidebar.blade.php
+++ resources/views/navigations/sidebar.blade.php
@@ -17,10 +17,9 @@
         </ul>
         <ul>
             <li class="relative px-6 py-3">
-                <span
-                    class="{{ Route::is('categories.index') ? 'absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg' : '' }}"
+                <span class="{{ Route::is('categories.*') ? 'absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg' : '' }}"
                     aria-hidden="true"></span>
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ Route::is('categories.index') ? 'text-gray-800 dark:text-gray-100' : '' }}"
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 {{ Route::is('categories.*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}"
                     href="{{ route('categories.index') }}">
                     <i class="fas fa-tags w-5 h-5"></i>
                     <span class="ml-4">Categories</span>
